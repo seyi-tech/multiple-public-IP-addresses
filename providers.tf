b1e0420bad8117c2e98ad84ce0c4251c2313228e
seyi-tech <110859626+seyi-tech@users.noreply.github.com>
2026-05-14 22:06:52 -0500

    providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.9"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>2.9"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      skip_shutdown_and_force_delete = true
+    }
+  }
+}
